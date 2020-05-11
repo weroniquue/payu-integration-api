57fcc9afc5d9e439d1651536bcc8e110b213985e
weroniquue <weronica97@gmail.com>
2020-05-11 21:27:20 +0200

    add authorization

--- routes/index.js
+++ routes/index.js
@@ -7,32 +7,51 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/pay', function(req, res, next) {
-	const headers = {
-		'Authorization': 'Bearer bbb36901-63a9-44e5-a42f-3aef9a53dd60',
-		'Content-Type': 'application/json',
-		'Access-Control-Allow-Origin': '*',
-		'Access-Control-Allow-Methods': "DELETE, POST, GET, OPTIONS",
-		"Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With",
-		'Access-Control-Allow-Visible-Redirect': '*'
-	};
+	const request = require('request');
 
-	const options = {
-		url: "https://secure.snd.payu.com/api/v2_1/orders",
-		headers: headers,
-		method: "POST",
-		body: req.body,
+	const authorizationUrl = 'https://secure.snd.payu.com/pl/standard/user/oauth/authorize?grant_type=client_credentials&client_id=' +
+			process.env.CLIENT_ID + '&client_secret=' + process.env.CLIENT_SECRET;
+
+	console.log(authorizationUrl);
+
+	const authOptions = {
+		url: authorizationUrl,
+		method: 'GET',
 		json: true
 	};
 
-	require('request').post(
-			options,
-			function(error, response, body){
-				if (error) {
-					res.send({error: "something went wrong"});
-				}
-				res.send(body);
-			}
-	);
+	request.get(authOptions, function (error, response, body) {
+		if (response.statusCode == 200) {
+
+			const headers = {
+				'Authorization': 'Bearer ' + body.access_token,
+				'Content-Type': 'application/json',
+				'Access-Control-Allow-Origin': '*',
+				'Access-Control-Allow-Methods': "DELETE, POST, GET, OPTIONS",
+				"Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With",
+				'Access-Control-Allow-Visible-Redirect': '*'
+			};
+
+			const options = {
+				url: "https://secure.snd.payu.com/api/v2_1/orders",
+				headers: headers,
+				method: "POST",
+				body: req.body,
+				json: true
+			};
+			request.post(
+					options,
+					function(error, response, body){
+						if (error) {
+							res.send({error: "something went wrong"});
+						}
+						res.send(body);
+					}
+			);
+		} else {
+			res.send({error: "something went wrong"});
+		}
+	});
 });
 
 module.exports = router;
